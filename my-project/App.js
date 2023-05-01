2a1e05ae05f5dfd2b1062e36d9b81cd0b7ef6080
Rodrigo Barros <brodrigo87@gmail.com>
2023-05-01 18:14:21 -0300

    logar

--- my-project/App.js
+++ my-project/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUp from './screens/SignUp';
-
+import SignIn from './screens/SignIn';
 
 
 export default function App() {
-  return <SignUp />
+  return <SignIn />
 }
 
 const styles = StyleSheet.create({
